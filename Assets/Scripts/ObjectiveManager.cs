5c2eaaf17a6a97654e06e09d66162dbac6b6b947
auttasit23 <auttasit.umpo@bumail.net>
2025-03-22 21:58:07 +0700

    Function Singleton

--- Assets/Scripts/ObjectiveManager.cs
+++ Assets/Scripts/ObjectiveManager.cs
@@ -8,15 +8,26 @@ public class ObjectiveManager : NetworkBehaviour
     
     public NetworkVariable<float> objective = new NetworkVariable<float>(0f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
     
+    public NetworkVariable<float> maxObjective = new NetworkVariable<float>(4f, NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+
+    public bool CheckObjectiveIsDone()
+    {
+        if (objective.Value >= maxObjective.Value)
+        {
+            return true;
+        }
+        return false;
+    }
     public void ObjectiveAdd()
     {
-        Debug.Log("Objective: " + objective.Value);
+        Debug.Log("Objective: " + objective.Value + "/" + maxObjective.Value);
         IncreaseValueServerRpc();
     }
     
     public void ObjectiveRemove()
     {
-        Debug.Log("Objective: " + objective.Value);
+        Debug.Log("Objective: " + objective.Value + "/" + maxObjective.Value);
         RemoveValueServerRpc();
     }
     
@@ -24,6 +35,14 @@ public class ObjectiveManager : NetworkBehaviour
     {
         if (Instance != null && Instance != this) { Destroy(gameObject); return; }
         Instance = this;
+    }
+    
+    private void Update()
+    {
+        if (IsServer)
+        {
+            UpdateMaxObjective();
+        }
     }
     
     [ServerRpc(RequireOwnership = false)]
@@ -35,7 +54,28 @@ public class ObjectiveManager : NetworkBehaviour
     [ServerRpc(RequireOwnership = false)]
     private void RemoveValueServerRpc()
     {
-        objective.Value -= 1f;
+        if (objective.Value > 0)
+        {
+            objective.Value -= 1f;
+        }
+    }
+    
+    private void UpdateMaxObjective()
+    {
+        int playerCount = NetworkManager.Singleton.ConnectedClients.Count;
+        
+        //Start 4
+        float baseMax = 4f;
+        //Calculate additional players
+        float additionalPlayers = Mathf.Max(0, playerCount - 4);
+        //Start 4 and plus with additional players
+        float newMax = baseMax + Mathf.Floor(additionalPlayers / 2f);
+        maxObjective.Value = newMax;
+
+        if (objective.Value > maxObjective.Value)
+        {
+            objective.Value = maxObjective.Value;
+        }
     }
     
     private void OnDestroy()
